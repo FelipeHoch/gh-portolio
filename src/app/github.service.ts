4ace66dd66bba7f49f230cc0dd5035c53f9ff761
Felipe Hoch <felipe.hoch@sirros.net>
2022-08-25 09:33:19 -0300

    Converts App to SSR.

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Organization } from './organization';
 import { Repository } from './repository';
 import { User } from './user';
 
@@ -23,5 +24,9 @@ export class GithubService {
 
   getRepos(): Observable<Repository[]> {
     return this.http.get<Repository[]>(this.userUrl + '/repos');
+  }
+
+  getOrganization(): Observable<Organization[]> {
+    return this.http.get<Organization[]>(this.userUrl + '/orgs');
   }
 }
