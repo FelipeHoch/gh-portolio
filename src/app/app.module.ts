e3fec6a98eb72b66ecb0143d66dbef70266fc7ed
Felipe Hoch <felipe.hoch@sirros.net>
2022-08-25 13:57:35 -0300

    Adds Preboot.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { HttpClientModule } from '@angular/common/http';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { PanelComponent } from './panel/panel.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
+import { PrebootModule } from 'preboot';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { OrganizationsComponent } from './organizations/organizations.component'
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    HttpClientModule
+    HttpClientModule,
+    TransferHttpCacheModule,
+    PrebootModule.withConfig( appRoot:'app-root')
   ],
   providers: [],
   bootstrap: [AppComponent]
